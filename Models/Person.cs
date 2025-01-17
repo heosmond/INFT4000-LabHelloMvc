ff91c949f682e06468c255f31dbd900fd63d3e1f
heosmond <osmondhe@gmail.com>
2025-01-17 13:57:05 -0400

    Completed

---
+++ Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace LabHelloMvc.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+    }
+}
